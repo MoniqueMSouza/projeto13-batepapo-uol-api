c19ae2cec5262e5d225602cad75ab7ce6118f041
MoniqueSouza <moniquemoreira.mkt@gmail.com>
2023-01-15 20:14:54 -0300

    Variável de Ambiente

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 import express from "express";
 import cors from "cors";
 import chalk from "chalk"
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv'
+dotenv.config()
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+let db;
+
+mongoClient.connect().then(() => {
+	db = mongoClient.db("bate-papo-uol-api"); 
+    console.log('Conectou com o mongodb!')
+});
+
 const app = express();
 app.use(cors());
 app.use(express.json());
